05de75d9590776b62c90afd914ff020218fa6d08
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-02-07 19:08:21 +0545

    first commit

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() {
+    return _UserImagePickerState();
+  }
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  void _pickImage(){
+    ImagePicker().pickImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150,);
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(children:[
+      CircleAvatar(
+        radius: 40,
+        backgroundColor: Colors.grey,
+        foregroundColor: ...,
+      ),
+      TextButton.icon(
+        onPressed: (){},
+        icon: const Icon(Icons.image),
+        label:Text(
+          "Add Image",
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+          ),
+        )
+      )
+    ]);
+  }
+}
