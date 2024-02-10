d7dd4c1573ec26838481f9911a839614584c393f
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-02-10 09:40:14 +0545

    fifth commit

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -26,7 +26,10 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        return ios;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.macOS:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for macos - '
@@ -55,14 +58,5 @@ class DefaultFirebaseOptions {
     messagingSenderId: '158228334023',
     projectId: 'chatapplication-2c49d',
     storageBucket: 'chatapplication-2c49d.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAHY0c6zRymiA-RUiXny_bb9iwS5Q2drc8',
-    appId: '1:158228334023:ios:1aec3f67af4c9bb20d05e2',
-    messagingSenderId: '158228334023',
-    projectId: 'chatapplication-2c49d',
-    storageBucket: 'chatapplication-2c49d.appspot.com',
-    iosBundleId: 'com.example.chatapplication',
   );
 }
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,8 +1,9 @@
-import 'package:chatapplication/widgets/chat_messages.dart';
-import 'package:chatapplication/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:chatapplication/widgets/chat_messages.dart';
+import 'package:chatapplication/widgets/new_messages.dart';
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
@@ -10,7 +11,22 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
+
 class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async{
+    final fcm= FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token= await fcm.getToken();
+    print(
+        token);
+
+  }
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
